0f48156630bf64e8c5f77ca3b047fe20feb324aa
brilliant kaimba <briegrant416@gmail.com>
2017-11-05 22:48:12 +0300

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-$("form#userInput").submit(function(event){
+ $("form#userInput").submit(function(event){
 $ ("#output ul").empty();
    var number = parseInt($("input").val());
 
    for (var index = 1; index<=number; index+=1) {
    	if (index%15==0) 
-   		{$("#output ul").append("<li>"+"Ping"+"</li>");} 
+   		{$("#output ul").append("<li>"+"pingpong"+"</li>");} 
    	else if (index%5 ==0) {$("#output ul").append("<li>"+"pong"+"</li>");}
-   	else if (index%3 ==0) {$("#output ul").append("<li>"+" pingpong"+"</li>")}
+   	else if (index%3 ==0) {$("#output ul").append("<li>"+" ping"+"</li>")}
 
    	else {$("#output ul").append("<li>" +index+ "</li>");}
    	
